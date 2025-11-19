4509f3fd7f7627214605206b7984a0087f0692ab
you <kamadayou10@gmail.com>
2025-11-19 16:35:16 +0900

    [Add]1 Scoreヘッダーを追加

---
+++ CollectTheFish/MainProject/Classes/Score.h
@@ -0,0 +1,19 @@
+
+//Score.h
+
+#pragma once
+
+#include "../HuEngine.h"
+
+class Score {
+public:
+    void Load();
+    void Initialize();
+    int  GetScore() const { return score_; }
+    void SetScore(int score);
+
+private:
+    int score_;
+    HE::SpriteFont score_headline_;
+    HE::SpriteFont score_text_;
+};
