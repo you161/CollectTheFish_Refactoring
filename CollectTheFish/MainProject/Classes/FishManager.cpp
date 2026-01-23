2abe556d44ea5f58dce9925bc58e9fd5b7b33e56
you <kamadayou10@gmail.com>
2026-01-23 18:39:36 +0900

    [Update] 2 魚管理スクリプトの更新

--- CollectTheFish/MainProject/Classes/FishManager.cpp
+++ CollectTheFish/MainProject/Classes/FishManager.cpp
@@ -7,3 +7,40 @@
 using namespace HE;
 using namespace Math;
 
+void FishManager::Load(string filename, int order)
+{
+	SpriteObject::Load(filename, order);
+}
+
+void FishManager::Initialize(Vector2 renderSize, Vector2 position)
+{
+	SpriteObject::Initialize(renderSize, position);
+	initial_position = position;
+}
+
+void FishManager::Update(float speed , int move)
+{
+	m_sprite.params.pos = fishMovement_.Movement(m_sprite.params.pos, m_sprite.params.siz, speed, move,
+		Vector2(-200.0f, Random::GetRandom(50.0f, 650.0f)));
+}
+
+CollisionRect FishManager::GetCollision()
+{
+	CollisionRect collision;
+	collision.x = (long)m_sprite.params.pos.x;
+	collision.y = (long)m_sprite.params.pos.y;
+	collision.width = (long)m_sprite.params.siz.x;
+	collision.height = (long)m_sprite.params.siz.y;
+
+	return collision;
+}
+
+void FishManager::OnCollision()
+{
+	SetPosition();
+}
+
+void FishManager::SetPosition()
+{
+	m_sprite.params.pos = initial_position;
+}
--- CollectTheFish/MainProject/Classes/FishManager.h
+++ CollectTheFish/MainProject/Classes/FishManager.h
@@ -3,14 +3,26 @@
 //
 
 #pragma once
-
 #include "../HuEngine.h"
+#include"SpriteObject.h"
+#include"ICollider.h"
+#include"FishMovement.h"
 
 using namespace HE;
 using namespace Math;
 using std::string;
 
-class FishManager {
+class FishManager : public SpriteObject, public ICollider {
 public:
+	void Load(string filename, int order);
+	void Initialize(Vector2 renderSize, Vector2 position);
+	void Update(float speed, int move);
+	void SetPosition();
+	CollisionRect GetCollision()override;
+	void OnCollision() override;
+
 protected:
+private:
+	FishMovement fishMovement_;
+	Vector2 initial_position;
 };
