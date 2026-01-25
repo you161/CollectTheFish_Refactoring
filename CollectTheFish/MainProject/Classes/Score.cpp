1f1bc42aab7aa4c12166acfeeaf95d76125231f5
you <kamadayou10@gmail.com>
2026-01-25 22:13:32 +0900

    [Update] 3 スコア表示位置の調整

--- CollectTheFish/MainProject/Classes/Score.cpp
+++ CollectTheFish/MainProject/Classes/Score.cpp
@@ -21,16 +21,16 @@ void Score::Initialize()
     score_ = 0;
 
     // スコア見出し(1-UP)
-    score_headline_.SetText(L"Score");
-    score_headline_.params.posX = 440.0f;
-    score_headline_.params.posY = 30.0f;
+    score_headline_.SetText(L"SCORE : ");
+    score_headline_.params.posX = 60.0f;
+    score_headline_.params.posY = 40.0f;
     score_headline_.params.size = 60;
     score_headline_.params.color = Color(255, 255, 255);    // 赤, 緑, 青(0-255)
 
     // スコア数値
     score_text_.SetText(std::to_string(score_));
-    score_text_.params.posX = 460.0f;
-    score_text_.params.posY = 80.0f;
+    score_text_.params.posX = 300.0f;
+    score_text_.params.posY = 30.0f;
     score_text_.params.size = 80;
     score_text_.params.color = Color(0, 0, 255);
 }
